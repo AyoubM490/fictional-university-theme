c48f8d678753ab5ef9b230aca5238ff725742c5a
AyoubMS <chahayb563@gmail.com>
2023-09-14 21:25:10 +0200

    add pages chapter 5

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
         <div class="group">
             <div class="site-footer__col-one">
                 <h1 class="school-logo-text school-logo-text--alt-color">
-                    <a href="#"><strong>Fictional</strong> University</a>
+                    <a href="<?php echo site_url('/') ?>"><strong>Fictional</strong> University</a>
                 </h1>
                 <p><a class="site-footer__link" href="#">555.555.5555</a></p>
             </div>
@@ -13,8 +13,13 @@
                 <div class="site-footer__col-two">
                     <h3 class="headline headline--small">Explore</h3>
                     <nav class="nav-list">
+<!--                        --><?php
+//                        wp_nav_menu(array(
+//                                'theme_location' => 'footerLocationOne'
+//                        ))
+//                        ?>
                         <ul>
-                            <li><a href="#">About Us</a></li>
+                            <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
                             <li><a href="#">Programs</a></li>
                             <li><a href="#">Events</a></li>
                             <li><a href="#">Campuses</a></li>
@@ -25,9 +30,14 @@
                 <div class="site-footer__col-three">
                     <h3 class="headline headline--small">Learn</h3>
                     <nav class="nav-list">
+<!--                        --><?php
+//                        wp_nav_menu(array(
+//                                'theme_location' => 'footerLocationTwo'
+//                        ))
+//                        ?>
                         <ul>
                             <li><a href="#">Legal</a></li>
-                            <li><a href="#">Privacy</a></li>
+                            <li><a href="<?php echo site_url('/privacy-policy') ?>">Privacy</a></li>
                             <li><a href="#">Careers</a></li>
                         </ul>
                     </nav>
--- header.php
+++ header.php
@@ -1,9 +1,11 @@
 <!doctype html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 <head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
 </head>
-<body>
+<body <?php body_class(); ?>>
 <header class="site-header">
     <div class="container">
         <h1 class="school-logo-text float-left">
@@ -13,8 +15,13 @@
         <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
         <div class="site-header__menu group">
             <nav class="main-navigation">
+<!--                --><?php
+//                wp_nav_menu(array(
+//                        'theme_location' => 'headerMenuLocation'
+//                ));
+//                ?>
                 <ul>
-                    <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
+                    <li <?php if (is_page('about-us') or wp_get_post_parent_id(0) === get_the_ID()) echo 'class="current-menu-item"' ?> ><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
                     <li><a href="#">Programs</a></li>
                     <li><a href="#">Events</a></li>
                     <li><a href="#">Campuses</a></li>
