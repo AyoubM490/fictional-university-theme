3b0341a1c05a7c4371fe2b90806458c4663d2192
AyoubMS <chahayb563@gmail.com>
2023-09-16 23:18:11 +0200

    events post type

--- single-event.php
+++ single-event.php
@@ -18,9 +18,9 @@ while(have_posts()) {
 
     <div class="container container--narrow page-section">
         <div class="metabox metabox--position-up metabox--with-home-link">
-            <p><a class="metabox__blog-home-link" href="<?php echo site_url('/blog') ?>"><i
-                            class="fa fa-home" aria-hidden="true"></i> Blog Home</a> <span
-                        class="metabox__main">Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', '); ?></span></p>
+            <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event') ?>"><i
+                        class="fa fa-home" aria-hidden="true"></i> Events Home</a> <span
+                    class="metabox__main"><?php the_title() ?></span></p>
         </div>
         <div class="generic-content"><?php the_content(); ?></div>
     </div>
