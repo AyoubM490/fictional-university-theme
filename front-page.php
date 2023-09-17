8d3daccaae56d156691344c4f5249c04d4551528
AyoubMS <chahayb563@gmail.com>
2023-09-17 22:28:59 +0200

    events post type

--- front-page.php
+++ front-page.php
@@ -20,9 +20,22 @@ get_header();
             <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
 
             <?php
+            $today = date('Ymd');
+
             $homepageEvents = new WP_Query(array(
                 'posts_per_page' => 2,
-                'post_type' => 'event'
+                'post_type' => 'event',
+                'meta_key' => 'event_date',
+                'orderby' => 'meta_value_num',
+                'order' => 'ASC',
+                'meta_query' => array(
+                        array(
+                            'key' => 'event_date',
+                            'compare' => '>=',
+                            'value' => $today,
+                            'type' => 'numeric'
+                        ),
+                )
             ));
 
             while ($homepageEvents->have_posts()) {
@@ -30,8 +43,11 @@ get_header();
 
                 <div class="event-summary">
                     <a class="event-summary__date t-center" href="#">
-                        <span class="event-summary__month">Mar</span>
-                        <span class="event-summary__day">25</span>
+                        <span class="event-summary__month"><?php
+                                $eventDate = new DateTime(get_field('event_date'));
+                                echo $eventDate->format('M');
+                            ?></span>
+                        <span class="event-summary__day"><?php echo $eventDate->format('d'); ?></span>
                     </a>
                     <div class="event-summary__content">
                         <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h5>
@@ -54,7 +70,7 @@ get_header();
             <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
             <?php
             $homepagePosts = new WP_Query(array(
-                'posts_per_page' => 2
+                'posts_per_page' => 2,
             ));
 
             while ($homepagePosts->have_posts()) {
--- page-past-events.php
+++ page-past-events.php
@@ -6,9 +6,9 @@ get_header(); ?>
         <div class="page-banner__bg-image"
              style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
         <div class="page-banner__content container container--narrow">
-            <h1 class="page-banner__title">All Events</h1>
+            <h1 class="page-banner__title">Past Events</h1>
             <div class="page-banner__intro">
-                <p>See what is going on in our world.</p>
+                <p>A recap of our past events.</p>
             </div>
         </div>
     </div>
@@ -16,8 +16,26 @@ get_header(); ?>
     <div class="container container--narrow page-section">
         <?php
 
-        while (have_posts()) {
-            the_post(); ?>
+        $today = date('Ymd');
+
+        $pastEvents = new WP_Query(array(
+            'paged' => get_query_var('paged', 1),
+            'post_type' => 'event',
+            'meta_key' => 'event_date',
+            'orderby' => 'meta_value_num',
+            'order' => 'ASC',
+            'meta_query' => array(
+                array(
+                    'key' => 'event_date',
+                    'compare' => '<',
+                    'value' => $today,
+                    'type' => 'numeric'
+                ),
+            )
+        ));
+
+        while ($pastEvents->have_posts()) {
+            $pastEvents->the_post(); ?>
 
             <div class="event-summary">
                 <a class="event-summary__date t-center" href="#">
@@ -36,12 +54,10 @@ get_header(); ?>
             </div>
 
         <?php }
-        echo paginate_links();
+        echo paginate_links(array(
+                'total' => $pastEvents->max_num_pages
+        ));
         ?>
-
-        <hr class="section-break" />
-
-        <p>Looking for a recap of past events? <a href="<?php echo site_url('/past-events'); ?>">Check out our past events archive.</a></p>
     </div>
 
 <?php get_footer();
