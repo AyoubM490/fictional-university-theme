ff96a70eaf3650872b24bad1810c7e689417e940
AyoubMS <chahayb563@gmail.com>
2023-09-18 22:19:51 +0200

    Programs Post Type

--- functions.php
+++ functions.php
@@ -23,6 +23,12 @@ add_action('after_setup_theme', 'university_features');
 
 function university_adjust_queries($query) {
     $today = date('Ymd');
+    if (!is_admin() and is_post_type_archive('program') and is_main_query()) {
+        $query->set('orderby', 'title');
+        $query->set('order', 'asc');
+        $query->set('posts_per_page', -1);
+    }
+
     if(!is_admin() and is_post_type_archive('event') and $query->is_main_query()) {
         $query->set('meta_key', 'event_date');
         $query->set('orderby', 'meta_value_num');
--- single-event.php
+++ single-event.php
@@ -2,7 +2,7 @@
 
 get_header();
 
-while(have_posts()) {
+while (have_posts()) {
     the_post(); ?>
 
     <div class="page-banner">
@@ -17,15 +17,31 @@ while(have_posts()) {
     </div>
 
     <div class="container container--narrow page-section">
-        <div class="metabox metabox--position-up metabox--with-home-link">
-            <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event') ?>"><i
+    <div class="metabox metabox--position-up metabox--with-home-link">
+        <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event') ?>"><i
                         class="fa fa-home" aria-hidden="true"></i> Events Home</a> <span
                     class="metabox__main"><?php the_title() ?></span></p>
-        </div>
-        <div class="generic-content"><?php the_content(); ?></div>
     </div>
 
-<?php }
+    <div class="generic-content"><?php the_content(); ?></div>
 
-get_footer();
-?>
+    <?php
+
+    $relatedPrograms = get_field('related_programs');
+
+    if ($relatedPrograms) {
+        echo '<hr class="section-break"/>';
+        echo '<h2 class="headline headline--medium">Related Program(s)</h2>';
+        echo '<ul class="link-list min-list">';
+        foreach ($relatedPrograms as $relatedProgram) { ?>
+            <li><a href="<?php get_the_permalink() ?>"><?php echo get_the_title($relatedProgram); ?></a></li>
+        <?php }
+        echo '</ul>';
+        }
+    ?>
+    </div>
+
+    <?php }
+
+    get_footer();
+    ?>
